9f7320566c9267c30e9f527f1ec651d1887e01e1
siddhiair <siddhiair8@gmail.com>
2025-07-15 10:38:12 +0200

    set up routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Test from "./Test";
+import { Header } from "./components/header";
+import { Home } from "./pages/homepage";
 
 function App() {
-  return <></>;
+  const RoutesSetup = () => {
+    return (
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    );
+  };
+
+  return (
+    <>
+      <Router>
+        <Header />
+        <RoutesSetup />
+      </Router>
+    </>
+  );
 }
 
 export default App;
