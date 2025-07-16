c5f117d3b1471e6914b4a130d19e05f721065486
siddhiair <siddhiair8@gmail.com>
2025-07-16 14:12:30 +0200

    added counter challenge

--- src/challenges/CounterComponent.tsx
+++ src/challenges/CounterComponent.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 const CounterComponent = () => {
-  return <div>CounterComponent</div>;
+  const [count, setCount] = useState<number>(0);
+
+  const increment = () => {
+    setCount((prev) => prev + 1);
+  };
+  const decrement = () => {
+    setCount((prev) => prev - 1);
+  };
+
+  const buttonClass =
+    "text-semibold text-white bg-black px-5 py-1 cursor-pointer transition-colors hover:bg-gray-700";
+
+  return (
+    <div className="border border-gray-400 px-4 py-4 flex justify-between items-center transition">
+      <span className="text-xl font-bold">{count}</span>
+      <div className="flex gap-x-3">
+        <button className={buttonClass} type="button" onClick={decrement}>
+          Decrease
+        </button>
+        <button className={buttonClass} type="button" onClick={increment}>
+          Increase
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default CounterComponent;
